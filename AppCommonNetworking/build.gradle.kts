7e7927e6dd0cdb241cb7d2d089257ab0cdd24ebe
arvifox <arvifox@hotmail.com>
2022-03-31 16:03:02 +0300

    test build

--- AppCommonNetworking/build.gradle.kts
+++ AppCommonNetworking/build.gradle.kts
@@ -11,6 +11,14 @@ buildscript {
     }
 }
 
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
