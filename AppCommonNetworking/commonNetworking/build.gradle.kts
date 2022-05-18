f8e82559fdec8555d35ecc74717fd5066ea4bcf5
Ivan Shlyapkin <shlyapkin01@gmail.com>
2022-05-18 13:12:51 +0400

    Update project settings

--- AppCommonNetworking/commonNetworking/build.gradle.kts
+++ AppCommonNetworking/commonNetworking/build.gradle.kts
@@ -8,14 +8,14 @@ plugins {
 
 group = "jp.co.soramitsu"
 
-version = "0.0.14"
+version = "0.0.15"
 
 publishing {
     publications {
         register<MavenPublication>("release") {
             groupId = "jp.co.soramitsu"
             artifactId = "common-networking"
-            version = "0.0.14"
+            version = "0.0.15"
 
             afterEvaluate {
                 from(components["release"])
