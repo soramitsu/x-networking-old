6a9dbbfb1db596644f841aac605b508d00dfad33
Ivan Shlyapkin <shlyapkin01@gmail.com>
2022-05-19 14:54:53 +0400

    Update name and depolyment target

--- AppCommonNetworking/commonNetworking/build.gradle.kts
+++ AppCommonNetworking/commonNetworking/build.gradle.kts
@@ -8,14 +8,14 @@ plugins {
 
 group = "jp.co.soramitsu"
 
-version = "0.0.15"
+version = "0.0.16"
 
 publishing {
     publications {
         register<MavenPublication>("release") {
             groupId = "jp.co.soramitsu"
             artifactId = "common-networking"
-            version = "0.0.15"
+            version = "0.0.16"
 
             afterEvaluate {
                 from(components["release"])
