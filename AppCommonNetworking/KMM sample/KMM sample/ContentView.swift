f247dc03017483db1732639b12254c7e0fa01608
Ivan Shlyapkin <shlyapkin01@gmail.com>
2022-05-25 18:45:29 +0400

    Updated sample app

--- AppCommonNetworking/KMM sample/KMM sample/ContentView.swift
+++ AppCommonNetworking/KMM sample/KMM sample/ContentView.swift
@@ -27,17 +27,19 @@ struct ContentView: View {
     }
     
     func load() {
-        let provider = SoraHttpClientProviderImpl()
-        var soraNetworkClient = SoraNetworkClient(timeout: 60000, logging: true, provider: provider)
-        
-        var fearlessChainsBuilder = FearlessChainsBuilder(networkClient: soraNetworkClient,
-                                                          baseUrl: "https://raw.githubusercontent.com/arvifox/arvifoxandroid/develop/felete/")
-        var hi = SubQueryClient(networkClient: soraNetworkClient,
-                                baseUrl: "https://api.subquery.network/sq/sora-xor/sora-dev")
-        hi.getReferrerRewards(address: "cnVkoGs3rEMqLqY27c2nfVXJRGdzNJk2ns78DcqtppaSRe8qm", blockHeight: 0, base: 18) { result, error in
-            print(result?.rewards)
+            let provider = SoraHttpClientProviderImpl()
+            var soraNetworkClient = SoraNetworkClient(timeout: 60000, logging: true, provider: provider)
+
+            var fearlessChainsBuilder = FearlessChainsBuilder(networkClient: soraNetworkClient,
+                                                              baseUrl: "https://raw.githubusercontent.com/arvifox/arvifoxandroid/develop/felete/")
+            
+            var hi = SubQueryClientFactory().create(soraNetworkClient: soraNetworkClient,
+                                                    baseUrl: "https://api.subquery.network/sq/sora-xor/sora-dev", pageSize: 30)
+            
+            hi.getReferrerRewards(address: "cnVkoGs3rEMqLqY27c2nfVXJRGdzNJk2ns78DcqtppaSRe8qm") { result, error in
+                print(result?.rewards)
+            }
         }
-    }
     
             
             
