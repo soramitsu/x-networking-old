257fa8390e6f5f1ec00ec8b37217497c02b958c1
Ivan Shlyapkin <shlyapkin01@gmail.com>
2022-05-25 13:30:39 +0400

    Finally

--- AppCommonNetworking/KMM sample/Pods/Target Support Files/X-Networking/X-Networking-xcframeworks.sh
+++ AppCommonNetworking/KMM sample/Pods/Target Support Files/X-Networking/X-Networking-xcframeworks.sh
@@ -17,11 +17,11 @@ RSYNC_PROTECT_TMP_FILES=(--filter "P .*.??????")
 variant_for_slice()
 {
   case "$1" in
-  "commonNetworking.xcframework/ios-x86_64-simulator")
-    echo "simulator"
-    ;;
   "commonNetworking.xcframework/ios-arm64")
     echo ""
+    ;;
+  "commonNetworking.xcframework/ios-arm64_x86_64-simulator")
+    echo "simulator"
     ;;
   esac
 }
@@ -29,11 +29,11 @@ variant_for_slice()
 archs_for_slice()
 {
   case "$1" in
-  "commonNetworking.xcframework/ios-x86_64-simulator")
-    echo "x86_64"
-    ;;
   "commonNetworking.xcframework/ios-arm64")
     echo "arm64"
+    ;;
+  "commonNetworking.xcframework/ios-arm64_x86_64-simulator")
+    echo "arm64 x86_64"
     ;;
   esac
 }
@@ -117,5 +117,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/X-Networking/AppCommonNetworking/commonNetworking/build/XCFrameworks/release/commonNetworking.xcframework" "X-Networking" "framework" "ios-x86_64-simulator" "ios-arm64"
+install_xcframework "${PODS_ROOT}/X-Networking/AppCommonNetworking/commonNetworking/build/XCFrameworks/release/commonNetworking.xcframework" "X-Networking" "framework" "ios-arm64" "ios-arm64_x86_64-simulator"
 
