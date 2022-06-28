5522bae490739496e13a7dfacbf71e51a03d8bf0
Arvi Fox <arvifox@arvifox.com>
2022-06-28 17:44:01 +0300

    mwr-37

--- AppCommonNetworking/commonNetworking/build.gradle.kts
+++ AppCommonNetworking/commonNetworking/build.gradle.kts
@@ -11,14 +11,14 @@ plugins {
 
 group = "jp.co.soramitsu"
 
-version = "0.0.23"
+version = "0.0.24"
 
 publishing {
     publications {
         register<MavenPublication>("release") {
             groupId = "jp.co.soramitsu"
             artifactId = "common-networking"
-            version = "0.0.23"
+            version = "0.0.24"
 
             afterEvaluate {
                 from(components["release"])
