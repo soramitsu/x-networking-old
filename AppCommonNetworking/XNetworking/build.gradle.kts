f99f72930924bfce5f4651f33e77b0d746f71b88
arvifox <arvifox@hotmail.com>
2022-11-07 16:50:23 +0300

    sbapy, fiat mappers

--- AppCommonNetworking/XNetworking/build.gradle.kts
+++ AppCommonNetworking/XNetworking/build.gradle.kts
@@ -11,14 +11,14 @@ plugins {
 
 group = "jp.co.soramitsu"
 
-version = "0.0.40"
+version = "0.0.41"
 
 publishing {
     publications {
         register<MavenPublication>("release") {
             groupId = "jp.co.soramitsu"
             artifactId = "xnetworking"
-            version = "0.0.40"
+            version = "0.0.41"
 
             afterEvaluate {
                 from(components["release"])
--- AppCommonNetworking/app/build.gradle.kts
+++ AppCommonNetworking/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.6.1")
-    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("com.google.android.material:material:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(project(mapOf("path" to ":XNetworking")))
 
