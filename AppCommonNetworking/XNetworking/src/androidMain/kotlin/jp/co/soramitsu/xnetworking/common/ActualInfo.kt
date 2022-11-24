9d7585fe2884ea72dcc9704f06114079ebf898b0
Vitaly Peryatin <peryatin.vitalik37@gmail.com>
2022-11-24 14:48:21 +0300

    MWR-296: Fix some errors for building iOS

--- AppCommonNetworking/XNetworking/src/androidMain/kotlin/jp/co/soramitsu/xnetworking/common/ActualInfo.kt
+++
@@ -1,5 +0,0 @@
-package jp.co.soramitsu.xnetworking.common
-
-internal actual fun platform(): String = "android"
-
-// content this.PackageManager.GetPackageInfo(this.PackageName, 0)
--- AppCommonNetworking/XNetworking/src/commonMain/kotlin/jp/co/soramitsu/xnetworking/common/ExpectInfo.kt
+++
@@ -1,3 +0,0 @@
-package jp.co.soramitsu.xnetworking.common
-
-internal expect fun platform(): String
