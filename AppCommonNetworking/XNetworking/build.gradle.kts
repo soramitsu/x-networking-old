126e3623c91d92566032a6c45f4e004fe7a894b0
arvifox <arvifox@hotmail.com>
2023-02-07 10:12:30 +0300

    fix fiat parsing

--- AppCommonNetworking/XNetworking/build.gradle.kts
+++ AppCommonNetworking/XNetworking/build.gradle.kts
@@ -11,14 +11,14 @@ plugins {
 
 group = "jp.co.soramitsu"
 
-version = "0.0.47"
+version = "0.0.48"
 
 publishing {
     publications {
         register<MavenPublication>("release") {
             groupId = "jp.co.soramitsu"
             artifactId = "xnetworking"
-            version = "0.0.47"
+            version = "0.0.48"
 
             afterEvaluate {
                 from(components["release"])
