6659b97a7a7e1b3a7d784c2ee4cd05ec6d578b79
AmadeyKuspakov <SteveDrumble1@gmail.com>
2023-09-10 17:29:38 +0400

    Removed intermediate folders

--- AppCommonNetworking/XNetworking/basic/src/iosMain/kotlin/jp/co/soramitsu/xnetworking/basic/common/ActualInfo.kt
+++
@@ -1,8 +0,0 @@
-package jp.co.soramitsu.xnetworking.basic.common
-
-import jp.co.soramitsu.xnetworking.basic.common.platform_ios
-
-actual fun platform(): String = platform_ios
-
-//CFBundleShortVersionString
-//internal actual fun version(): String = NSBundle.mainBundle.objectForInfoDictionaryKey("CFBundleVersion").toString()
---
+++ app/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("plugin.serialization")
+}
+
+android {
+    compileSdk = 33
+    defaultConfig {
+        applicationId = "jp.co.soramitsu.appxnetworking"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.6"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    namespace = "jp.co.soramitsu.appxnetworking"
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_1_8.toString()
+        }
+    }
+}
+
+dependencies {
+    implementation(project(mapOf("path" to ":core:basic")))
+    implementation(project(mapOf("path" to ":core:sorawallet")))
+    implementation(project(mapOf("path" to ":core:fearlesswallet")))
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.ui:ui:1.4.3")
+    implementation("androidx.compose.material:material:1.4.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.3")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.3")
+
+    //test
+    implementation("androidx.test.ext:junit:1.1.5")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("io.mockk:mockk:1.13.5")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+    testImplementation("io.ktor:ktor-client-mock:2.2.3")
+    testImplementation("io.ktor:ktor-client-content-negotiation:2.2.3")
+    testImplementation("io.ktor:ktor-serialization-kotlinx-json:2.2.3")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.work:work-testing:2.8.1")
+    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
