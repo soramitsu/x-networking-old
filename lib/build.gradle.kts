078602a6ab89305c8dbbc64813dd41e625bf4c5a
C4tWithShell <shelkovnikov@soramitsu.co.jp>
2024-05-15 12:15:25 +0300

    Add kover

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -13,6 +13,8 @@ plugins {
     id("com.apollographql.apollo3")
 
     id("com.google.devtools.ksp")
+
+    id("org.jetbrains.kotlinx.kover")
 }
 
 val libVersion: String by project
@@ -239,3 +241,53 @@ tasks.register<Copy>("copyiOSTestResources") {
 }
 
 tasks.findByName("iosX64Test")!!.dependsOn("copyiOSTestResources")
+
+kover {
+    useJacoco()
+}
+
+koverReport {
+    androidReports("release") {
+        filters {
+            excludes {
+                classes(
+                    "*.BuildConfig",
+                    "**.models.*",
+                    "**.core.network.*",
+                    "**.di.*",
+                    "**.shared_utils.wsrpc.*",
+                    "*NetworkDataSource",
+                    "*NetworkDataSource\$*",
+                    "*ChainConnection",
+                    "*ChainConnection\$*",
+                    "**.runtime.definitions.TypeDefinitionsTreeV2",
+                    "**.runtime.definitions.TypeDefinitionsTreeV2\$*",
+
+                    // TODO: Coverage these modules by tests
+                    "**.core.rpc.*",
+                    "**.core.utils.*",
+                    "**.core.extrinsic.*",
+                )
+            }
+        }
+
+        xml {
+            onCheck = true
+            setReportFile(file("${project.rootDir}/report/coverage.xml"))
+        }
+
+        html {
+            onCheck = true
+        }
+
+        verify {
+            onCheck = true
+
+            rule {
+                isEnabled = true
+
+                minBound(5)
+            }
+        }
+    }
+}
